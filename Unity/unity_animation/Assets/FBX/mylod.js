12508e807c2d1e2551a3f88aa8e19beb771c0d0e
OmegaArt <chelseygobeli@tamu.edu>
2014-11-20 20:29:25 -0600

    unity

---
+++ Unity/unity_animation/Assets/FBX/mylod.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+
+public var lowMesh : Transform;
+
+public var custDist : float; 
+
+private var dist : float;
+
+function Start () {
+
+}
+
+function Update () {
+
+// Check to see how far low mesh is to  controler 
+
+ 	if (lowMesh) 
+ 	{
+ 		dist = Vector3.Distance(lowMesh.position, transform.position);
+ 		
+ 		print ('Distance to other' + dist);		
+	}
+
+// Turn of low mesh if distance is close
+
+	if ( dist <= custDist ) {
+	
+		 gameObject.SetActive (false );
+	
+	}
+	
+// Turn low mesh back on if distance is grater than custDist
+
+	if (dist >= custDist ) {
+	
+		renderer.enabled = true ;
+		
+	}
+	
+}
+
+
+
+//Draw
+
+function OnGUI() {
+
+	GUI.Box(Rect(0,0,Screen.width,Screen.height), 'Dist = ' + dist);
+	
+	}
---
+++ Unity/unity_animation/Assets/LOD_Controller.js
@@ -0,0 +1,61 @@
+﻿#pragma strict
+
+public var distanceRanges : float[];
+public var lodModels : GameObject[];
+
+private var current = -2 ;
+
+
+
+public var PlayerCamera : Camera;
+
+function Start () {
+
+for (var i = 0; i < lodModels.Length; i++)
+	{
+		lodModels[i].SetActiveRecursively(false);
+	}
+
+}
+
+function Update () {
+
+	var d = Vector3.Distance(PlayerCamera.main.transform.position, transform.position) ;
+	
+	var level = -1 ;
+	
+	for (var i = 0; i < distanceRanges.Length; i++)
+	{
+		if (d< distanceRanges[i])
+		{
+			level = i;
+			i = distanceRanges.Length;
+		}
+		
+		if (level == -1)
+		{
+			level = distanceRanges.Length;
+		}
+		
+		if (current !=level)
+		{
+			ChangeLOD(level);
+		}
+	}
+	
+	}
+	
+	function ChangeLOD (level)
+	
+	{
+	
+		lodModels[level].SetActiveRecursively(true);
+		
+		if( current >= 0)
+		{
+			lodModels[current].SetActiveRecursively(false);	
+		}
+	
+		current = level ;
+
+}
